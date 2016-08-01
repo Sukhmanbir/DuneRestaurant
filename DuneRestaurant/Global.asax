ae93ba504696bb713913684b6c3ec246c163ca4a
naes <naes@NAES-PC>
2016-07-31 23:01:36 -0400

    Add project files.

---
+++ DuneRestaurant/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DuneRestaurant.MvcApplication" Language="C#" %>
---
+++ DuneRestaurant/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DuneRestaurant.Startup))]
+namespace DuneRestaurant
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
