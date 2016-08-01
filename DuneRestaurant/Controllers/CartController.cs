d26c4aa5aff501f29c689737180214207d4d14e5
Douglas Brunner <code@douglasbrunner.name>
2016-08-01 00:27:23 -0400

    Added stub pages

---
+++ DuneRestaurant/Controllers/CartController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DuneRestaurant.Controllers
+{
+    public class CartController : Controller
+    {
+        // GET: Cart
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Delete() {
+            return View();
+        }
+
+        public ActionResult Add() {
+            return View();
+        }
+
+        public ActionResult Update() {
+            return View();
+        }
+
+        public ActionResult Checkout() {
+            return View();
+        }
+
+        public ActionResult ConfirmOrder() {
+            return View();
+        }
+        
+    }
+}
---
+++ DuneRestaurant/Controllers/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DuneRestaurant.Controllers
+{
+    public class MenuController : Controller
+    {
+        // GET: Menu
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Dish() {
+            return View();
+        }
+    }
+}
