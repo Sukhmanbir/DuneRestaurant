504f70e7b0d973f76d000612780ec8a90b9da28e
Douglas Brunner <code@douglasbrunner.name>
2016-08-14 02:05:45 -0400

    Add items to cart

--- DuneRestaurant/Controllers/CartController.cs
+++ DuneRestaurant/Controllers/CartController.cs
@@ -8,6 +8,7 @@
         display cart information from CartView
         update cart quantity
         delete cart item
+        added items to cart
 */
 using System;
 using System.Collections.Generic;
@@ -16,12 +17,18 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Microsoft.AspNet.Identity;
 using DuneRestaurant.Models;
 
 namespace DuneRestaurant.Controllers
 {
     public class CartController : Controller
     {
+
+        // holds random number generator
+        private static Random random = new Random();
+        public readonly DateTime UnixEpoch = new DateTime(1970, 1, 1);
+
         //establish a class wide azure connection
         AzureConnection azure = new AzureConnection();
 
@@ -66,9 +73,76 @@ namespace DuneRestaurant.Controllers
 
         }
 
+        /// <summary>
+        ///  adds an dish to the cart, or updates the quantity if already present
+        /// </summary>
         public ActionResult Add() {
+
+            // get the dish id
+            string dishIDString = Request["dishId"];
+
+            // do nothing if the dish id is missing
+            if (string.IsNullOrEmpty(dishIDString) ||
+                string.IsNullOrWhiteSpace(dishIDString))
+            {
+                Response.Redirect("/Menu");
+            }
+
+            // try to convert the dishID to a string
+            int dishID = -1;
+            if (!Int32.TryParse(dishIDString, out dishID)) { Response.Redirect("/Dish"); }
+            
+            // get the user id if present, if not, generate a random ID and store it in a session
+            // this random session id will be used to identify the user until they login
+            var userID = User.Identity.GetUserId();
+
+            // there is no userID or sessionID
+            if (userID == null && Session["userID"] == null)
+            {
+                int randomID = random.Next(0, 100);
+                Session["userID"] = randomID;
+                userID = Convert.ToString(randomID);
+
+                // there is no userID but there is a SessionID
+            } else if (userID == null && Session["userID"] != null) {
+
+                userID = Session["userID"].ToString();
+
+            }
+
+                using (AzureConnection db = new AzureConnection())
+            {
+
+                // attempt to get an existing record
+                Cart cartModel = (from Cart in db.Carts
+                                 where Cart.UserID == userID
+                                 where Cart.DishID == dishID
+                                 select Cart).FirstOrDefault();
+
+                // add the dish to the cart if nothing was returned
+                if (cartModel == null) {
+
+                    // create a new model and add that to the database
+                    cartModel = new Cart();
+
+                    cartModel.DishID = dishID;
+                    cartModel.UserID = userID;
+                    db.Carts.Add(cartModel);
+                    db.SaveChanges();
+
+                // just update the quantity if the entry was found
+                } else {
+                    cartModel.Quantity += 1;
+                    db.SaveChanges();
+                }
+                
+            }
+
             return View();
+                
         }
+
+        /*
         public async Task<ActionResult> Add([Bind(Include = "DishID,CartID,UserId,Quantity")] Cart cart)
         {
             //add the dish to the cart
@@ -82,6 +156,7 @@ namespace DuneRestaurant.Controllers
 
             return View(cart);
         }
+        */
 
         [HttpPost]
         public void Update() {
